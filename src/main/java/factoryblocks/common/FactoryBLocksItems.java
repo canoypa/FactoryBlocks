234de4fbd1d47a3bd6ee77a07d3b2fe466339c21
Cano <developer.sasazono@gmail.com>
2019-09-16 16:53:27 +0900

    アイテム登録用クラスの追加

---
+++ src/main/java/factoryblocks/common/FactoryBLocksItems.java
@@ -0,0 +1,30 @@
+package factoryblocks.common;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.registries.IForgeRegistry;
+
+@ObjectHolder(FactoryBlocks.MODID)
+public class FactoryBLocksItems {
+
+  public static final void registerItems(IForgeRegistry<Item> registry) {
+    final Item[] items = {};
+
+    registry.registerAll(items);
+  }
+
+  public static final void registerItemBlocks(IForgeRegistry<Item> registry) {
+    final Item[] blockItems = {
+        new ItemBlock(FactoryBlocksBlocks.catwalks).setRegistryName(FactoryBlocksBlocks.catwalks.getRegistryName()) };
+
+    registry.registerAll(blockItems);
+  }
+
+  private static final Item init(Item item, String registryName, CreativeTabs tab) {
+    return item.setRegistryName(FactoryBlocks.MODID, registryName)
+        .setTranslationKey(FactoryBlocks.MODID + "." + registryName).setCreativeTab(tab);
+  }
+
+}
