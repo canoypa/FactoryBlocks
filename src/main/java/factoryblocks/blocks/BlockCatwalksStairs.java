9e0e97568da58ed4771122a4097005565c7a420c
Cano <developer.sasazono@gmail.com>
2019-09-17 18:54:10 +0900

    Stairs クラスを追加

---
+++ src/main/java/factoryblocks/blocks/BlockCatwalksStairs.java
@@ -0,0 +1,15 @@
+package factoryblocks.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class BlockCatwalksStairs extends Block {
+
+  public BlockCatwalksStairs() {
+    super(Material.IRON);
+
+    setSoundType(SoundType.STONE);
+  }
+
+}
