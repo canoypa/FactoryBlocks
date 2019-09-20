1656207376523a179052a2ef6e7d1fa3ba03700c
Cano <developer.sasazono@gmail.com>
2019-09-20 12:43:33 +0900

    OBJLoader の登録を追加

--- src/main/java/factoryblocks/common/FactoryBlocks.java
+++ src/main/java/factoryblocks/common/FactoryBlocks.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -28,6 +29,10 @@ public class FactoryBlocks {
   @EventHandler
   public void preInit(FMLPreInitializationEvent event) {
     MinecraftForge.EVENT_BUS.register(new EventBusFactoryBlocks());
+
+    if (event.getSide().isClient()) {
+      OBJLoader.INSTANCE.addDomain(MODID); // モデルのあるドメインを登録
+    }
   }
 
   @EventHandler
