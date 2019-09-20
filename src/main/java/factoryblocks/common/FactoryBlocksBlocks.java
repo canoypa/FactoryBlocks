da48eb17e7a303068f2badd82416b4f445acbe6a
Cano <developer.sasazono@gmail.com>
2019-09-20 12:41:12 +0900

    catwalks stairs のレジストリへの登録を追加

--- src/main/java/factoryblocks/common/FactoryBlocksBlocks.java
+++ src/main/java/factoryblocks/common/FactoryBlocksBlocks.java
@@ -1,6 +1,7 @@
 package factoryblocks.common;
 
 import factoryblocks.blocks.BlockCatwalks;
+import factoryblocks.blocks.BlockCatwalksStairs;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
@@ -10,9 +11,11 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class FactoryBlocksBlocks {
 
   public static final Block catwalks = null;
+  public static final Block catwalksStairs = null;
 
   public static final void registerBlocks(IForgeRegistry<Block> registry) {
-    final Block[] blocks = { init(new BlockCatwalks(), "catwalks", FactoryBlocks.creativeTabFactoryBlocks) };
+    final Block[] blocks = { init(new BlockCatwalks(), "catwalks", FactoryBlocks.creativeTabFactoryBlocks),
+        init(new BlockCatwalksStairs(), "catwalksStairs", FactoryBlocks.creativeTabFactoryBlocks) };
 
     registry.registerAll(blocks);
   }
