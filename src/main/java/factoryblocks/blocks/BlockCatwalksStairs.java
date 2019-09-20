158dc0424e91528e6c997afbe34b7b0253d21fa4
Cano <developer.sasazono@gmail.com>
2019-09-20 16:57:51 +0900

    ブロック追加時の基本処理を追加

--- src/main/java/factoryblocks/blocks/BlockCatwalksStairs.java
+++ src/main/java/factoryblocks/blocks/BlockCatwalksStairs.java
@@ -1,12 +1,18 @@
 package factoryblocks.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockCatwalksStairs extends BlockCatwalksBase {
 
@@ -22,6 +28,27 @@ public class BlockCatwalksStairs extends BlockCatwalksBase {
         .withProperty(Ascending, false).withProperty(Descent, false);
 
     this.setDefaultState(defaultState);
+  }
+
+  @Override
+  public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+    world.setBlockState(pos, state.withProperty(Facing, placer.getHorizontalFacing()));
+
+    super.onBlockPlacedBy(world, pos, state, placer, stack);
+  }
+
+  @Override
+  public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
+    world.setBlockState(pos, this.getActualState(state, world, pos));
+
+    super.onBlockAdded(world, pos, state);
+  }
+
+  @Override
+  public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+    // Block blockNorth = world.getBlockState(pos.north()).getBlock();
+
+    return state.withProperty(Ascending, false).withProperty(Descent, false);
   }
 
   // blockstate
