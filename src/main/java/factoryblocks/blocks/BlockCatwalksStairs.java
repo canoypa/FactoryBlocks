4e38a3d57e07ccd1b3124c1358e7155793151bc0
Cano <developer.sasazono@gmail.com>
2019-09-20 12:58:33 +0900

    catwalks stairs にプロパティを追加

--- src/main/java/factoryblocks/blocks/BlockCatwalksStairs.java
+++ src/main/java/factoryblocks/blocks/BlockCatwalksStairs.java
@@ -1,9 +1,24 @@
 package factoryblocks.blocks;
 
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+
 public class BlockCatwalksStairs extends BlockCatwalksBase {
+
+  // ブロックのステータスプロパティ
+  private static final PropertyBool Facing = PropertyBool.create("facing"); // TODO
+  private static final PropertyBool Ascending = PropertyBool.create("ascending");
+  private static final PropertyBool Descent = PropertyBool.create("descent");
 
   public BlockCatwalksStairs() {
     super();
+  }
+
+  // blockstate
+  @Override
+  protected BlockStateContainer createBlockState() {
+    return new BlockStateContainer(this, new IProperty[] { Facing, Ascending, Descent });
   }
 
 }
