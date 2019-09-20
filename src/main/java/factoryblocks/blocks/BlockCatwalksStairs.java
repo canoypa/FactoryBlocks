f5d8d21c7e643124312811331ff0a6c9d4435f76
Cano <developer.sasazono@gmail.com>
2019-09-20 16:52:14 +0900

    facing プロパティの型を PropertyDirection に変更

--- src/main/java/factoryblocks/blocks/BlockCatwalksStairs.java
+++ src/main/java/factoryblocks/blocks/BlockCatwalksStairs.java
@@ -1,13 +1,15 @@
 package factoryblocks.blocks;
 
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 
 public class BlockCatwalksStairs extends BlockCatwalksBase {
 
   // ブロックのステータスプロパティ
-  private static final PropertyBool Facing = PropertyBool.create("facing"); // TODO
+  private static final PropertyDirection Facing = BlockHorizontal.FACING;
   private static final PropertyBool Ascending = PropertyBool.create("ascending");
   private static final PropertyBool Descent = PropertyBool.create("descent");
 
